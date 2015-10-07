94101b4b85a6362c51f29cb34d3c169826c1a8cf
George Keith <ggkeithv@gmail.com>
2015-10-06 21:20:53 -0500

    initial project commit

---
+++ RandomQuote/js/index.js
@@ -0,0 +1,58 @@
+//create array of quotes (at least 20-30 quotes)
+$(document).ready(function() {
+
+  var quotes = [{
+    quote: "I met him, fifteen years ago; I was told there was nothing left; no reason, no conscience, no understanding; and even the most rudimentary sense of life or death, of good or evil, right or wrong. I met this six-year-old child, with this blank, pale, emotionless face, and the blackest eyes... the devil's eyes. I spent eight years trying to reach him, and then another seven trying to keep him locked up because I realized that what was living behind that boy's eyes was purely and simply... evil.",
+    name: "Dr. Samuel Loomis"
+  }, {
+    quote: "Death has come to your little town, sheriff.",
+    name: "Dr. Samuel Loomis"
+  }, {
+    quote: "Inside every one us, there exists a dark side. Most people rise above it, but some are consumed by it. Until there is nothing left, but pure evil.",
+    name: "Dr. Samuel Loomis"
+  }, {
+    quote: "All right, I'll stay with you tonight. Just for the chance that you are right. And if you are right, damn you for letting him go.",
+    name: "Sheriff Leigh Brackett"
+  }, {
+    quote: "The darkest souls are not those which choose to exist within the hell of the abyss, but those which choose to move silently among us.",
+    name: "Dr. Samuel Loomis"
+  }, {
+    quote: "Are you saying Michael did this? Michael loves animals!",
+    name: "Deborah Myers"
+  }, {
+    quote: "Sleep tight kids.",
+    name: "Laurie Strode"
+  }, {
+    quote: "I heard that on Halloween night, the boogeyman comes out at night and attacks kids.",
+    name: "Tommy Doyle"
+  }, {
+    quote: "These eyes will deceive you, they will destroy you. They will take from you, your innocence, your pride, and eventually your soul. These eyes do not see what you and I see. Behind these eyes one finds only blackness, the absence of light, these are of a psychopath.",
+    name: "Dr. Samuel Loomis"
+  }, {
+    quote: "He has come back for his baby sister.",
+    name: "Dr. Samuel Loomis"
+  }, {
+    quote: "Every kid in Haddonfield thinks this place is haunted.",
+    name: "Sheriff Leigh Brackett"
+  }, {
+    quote: "Was that the boogeyman?",
+    name: "Laurie Strode"
+  }, {
+    quote: "It's Halloween, everyone's entitled to one good scare.",
+    name: "Sheriff Leigh Brackett"
+  }, {
+    quote: "I- I- I watched him for fifteen years, sitting in a room, staring at a wall, not seeing the wall, looking past the wall - looking at this night, inhumanly patient, waiting for some secret, silent alarm to trigger him off. Death has come to your little town, Sheriff. Now you can either ignore it, or you can help me to stop it.",
+    name: "Dr. Samuel Loomis"
+  }];
+//create a random function that chooses a random quote from the array
+ 
+//when button clicked, print random quote to the page
+  $("#quoteButton").click(function(){
+    var quoteLength = quotes.length;
+    for (var i = 0; i < quoteLength; i++){
+      $("#quoteContainer p").append
+    }
+    
+  });
+});
+//create footer with my contact information
