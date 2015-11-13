389d0804aa8d77b73afbed0904cda641012dde02
George Keith <ggkeithv@gmail.com>
2015-11-12 18:32:26 -0600

    added app.js for Angular

---
+++ Portfolio/js/app.js
@@ -0,0 +1,20 @@
+(function () {
+    var app = angular.module("portfolio", []);
+
+    app.controller("IntroController", function() {
+
+    });
+
+    var me = {
+        name: "George Keith",
+        position: "Web Developer",
+    }
+
+    var skills = {
+        
+    }
+
+
+
+
+})();
