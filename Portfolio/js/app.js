2c0e5e828665de6d2fdf11e017d023aad7b290e9
George Keith <ggkeithv@gmail.com>
2015-11-13 08:52:43 -0600

    adding angular to page

--- Portfolio/js/app.js
+++ Portfolio/js/app.js
@@ -2,16 +2,18 @@
     var app = angular.module("portfolio", []);
 
     app.controller("IntroController", function() {
-
+        this.person = me;
+        this.sites = contactSites;
     });
 
     var me = {
         name: "George Keith",
         position: "Web Developer",
+
     }
 
-    var skills = {
-        
+    var contactSites = {
+
     }
 
 
